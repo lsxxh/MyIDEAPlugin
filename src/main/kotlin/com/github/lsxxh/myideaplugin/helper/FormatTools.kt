4ee7aee9bd1cd40f17f552f73467fb838cb86ea8
Zylsxxh <3477079189@qq.com>
2022-12-11 22:00:46 +0800

    成功根据settings中设置的倍率转换

--- src/main/kotlin/com/github/lsxxh/myideaplugin/helper/FormatTools.kt
+++ src/main/kotlin/com/github/lsxxh/myideaplugin/helper/FormatTools.kt
@@ -2,15 +2,19 @@ package com.github.lsxxh.myideaplugin.helper
 
 import com.example.plugindemo.constvalue.ConstValue.CHAR_QUOTE
 import com.example.plugindemo.model.ActionPerformer
+import com.github.lsxxh.myideaplugin.settings.PreferenceVariant
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.util.TextRange
 import java.util.regex.Pattern
+import kotlin.properties.Delegates
 
 
 private fun String.getNumericStr(): String {
     FormatTools.logger.warn("yyz, this: $this") //WARN - yideaplugin.helper.FormatTools - yyz, this: 10px"
     val matcher = Pattern.compile("-?[0-9]*(\\.[0-9]+)?").matcher(this)
+    //注意:想要matcher.group()方法，必须先matcher.find()方法，切忌！
+    //要不然总是报：java.lang.IllegalStateException: No match found
     while (matcher.find()) {
         FormatTools.logger.warn("yyz, matcher.group(): ${matcher.group()}")
         return matcher.group()
@@ -21,7 +25,11 @@ private fun String.getNumericStr(): String {
 object FormatTools{
     //Need: import com.intellij.openapi.diagnostic.Logger
     val logger = Logger.getInstance(FormatTools::class.java)
-    private var preset_density_scale = 0.5
+    //只有第一次起了作用,后面都是用的第一次设置的值,故用kt 委托
+    //但委托是提供一个回调当观测的这个值改变时回调;此处直接在每次转换时获取该值即可
+    /*private var preset_density_scale: Float by Delegates.observable(0.5f){
+        property, oldValue, newValue -> print("property: $property $oldValue -> $newValue")
+    }*/
 
     /**
      * 格式化光标处往前最近的一个可格化的长度
@@ -47,33 +55,34 @@ object FormatTools{
             )
         }
     }
+
 //    /**
 //     * 格式化光标处往前最近的一个可格化的长度
 //     *
 //     * @param actionPerformer 获取的动作参数
 //     */
-//    @JvmStatic
-//    fun formatCaretBeforeCode(actionPerformer: ActionPerformer) {
-//        val document = actionPerformer.document
-//        val caretModel = actionPerformer.caretModel
-//        val lineNum = caretModel?.let { document?.getLineNumber(it.offset) }
-//        val lineStartOffset = lineNum?.let { document?.getLineStartOffset(it) }
-//        val lineContent = document?.getText(TextRange(lineStartOffset!!, caretModel.offset))
-//        //  xx="10px or xx="10px" or xx="10
-//        val hasPXUnitAppended = lineContent!!.contains("px") && lineContent.length - lineContent.indexOf("px") <= 3
-//        val content = lineContent.substring(getCaretBeforeCodeIndex(lineContent) + 1, lineContent.length
-//                - if (hasPXUnitAppended) 2 else 0)
-//        // LOG.debug("px: $hasPXUnitAppended content: $content") //无效
-//        if (caretModel != null) {
-//            formatText(
-//                    content,
-//                    //arrayOf(caretModel.offset - content.length - 2, caretModel.offset),
-//                    arrayOf(caretModel.offset - content.length, caretModel.offset),
-//                    actionPerformer
-//            )
-//        }
-//    }
-//
+    /*
+    @JvmStatic
+    fun formatCaretBeforeCode(actionPerformer: ActionPerformer) {
+        val document = actionPerformer.document
+        val caretModel = actionPerformer.caretModel
+        val lineNum = caretModel?.let { document?.getLineNumber(it.offset) }
+        val lineStartOffset = lineNum?.let { document?.getLineStartOffset(it) }
+        val lineContent = document?.getText(TextRange(lineStartOffset!!, caretModel.offset))
+        //  xx="10px or xx="10px" or xx="10
+        val hasPXUnitAppended = lineContent!!.contains("px") && lineContent.length - lineContent.indexOf("px") <= 3
+        val content = lineContent.substring(getCaretBeforeCodeIndex(lineContent) + 1, lineContent.length
+                - if (hasPXUnitAppended) 2 else 0)
+        // LOG.debug("px: $hasPXUnitAppended content: $content") //无效
+        if (caretModel != null) {
+            formatText(
+                    content,
+                    //arrayOf(caretModel.offset - content.length - 2, caretModel.offset),
+                    arrayOf(caretModel.offset - content.length, caretModel.offset),
+                    actionPerformer
+            )
+        }
+    }*/
 
     /**
      * 格式化指定的style文本
@@ -89,32 +98,33 @@ object FormatTools{
                 position[0],
                 position[1],
                 //成功调用formatCaretBeforeCode测试将: xx="5px=>xx="50dp
-                (content.getNumericStr().toInt() * preset_density_scale).toInt().toString() + "dp\""
+                (content.getNumericStr().toInt() * PreferenceVariant.presetScale).toInt().toString() + "dp\""
             )
         }
         logger.warn("yyz, content.getNumericStr(): ${content.getNumericStr()}")
+        logger.warn("yyz, scale: ${PreferenceVariant.presetScale}")
     }
-//    /**
-// * 格式化指定的style文本
-// *
-// * @param content           style文本
-// * @param position        起止index
-// * @param actionPerformer 动作参数
-// */
-//private fun formatText(content: String, position: Array<Int>, actionPerformer: ActionPerformer) {
-//    //filter所要求的写法FormatTools::isNumeric要求一个类文件，顶层文件不行
-//    Optional.of(content).filter(FormatTools::isNumeric).ifPresent {
-//        WriteCommandAction.runWriteCommandAction(actionPerformer.project) {
-//            actionPerformer.document?.replaceString(
-//                position[0],
-//                position[1],
-//                //成功调用formatCaretBeforeCode测试将: xx="5px=>xx="50dp
-//                (content.toInt() * preset_density_scale).toInt().toString() + "dp\""
-//            )
-//        }
-//    }
-//}
 
+    //    /**
+    // * 格式化指定的style文本
+    // *
+    // * @param content           style文本
+    // * @param position        起止index
+    // * @param actionPerformer 动作参数
+    // */
+    //private fun formatText(content: String, position: Array<Int>, actionPerformer: ActionPerformer) {
+    //    //filter所要求的写法FormatTools::isNumeric要求一个类文件，顶层文件不行
+    //    Optional.of(content).filter(FormatTools::isNumeric).ifPresent {
+    //        WriteCommandAction.runWriteCommandAction(actionPerformer.project) {
+    //            actionPerformer.document?.replaceString(
+    //                position[0],
+    //                position[1],
+    //                //成功调用formatCaretBeforeCode测试将: xx="5px=>xx="50dp
+    //                (content.toInt() * preset_density_scale).toInt().toString() + "dp\""
+    //            )
+    //        }
+    //    }
+    //}
 
     private fun isNumeric(str: String) =
             Pattern.compile("-?[0-9]*(\\.[0-9]+)?").matcher(str).matches()
